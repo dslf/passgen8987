d9a4bf82bef0d00ed61d22f938c61bb695e0acbf
Denis Lafin <dslf@live.ru>
2025-02-07 02:15:05 +0300

    fisher yates shuffle

--- passhefn.c
+++ passhefn.c
@@ -9,10 +9,24 @@
 #define DIGIT 4
 #define SPECI 8
 
+void shuffle_combined(char *combi){
+    int len = strlen(combi);
+    int j;
+    srand(time(NULL));
+
+	for (int i = 0; i < len; i++){
+		j = i + rand() % (len-i);
+		int temp = combi[i];
+		combi[i] = combi[j];
+		combi[j] = temp;
+	}
+
+}
+
 int main(){
 
-    int bit_mask = LOWER;
-    int passwd_length = 20;
+    int bit_mask = LOWER+UPPER+DIGIT;
+    int passwd_length = 120;
     int passwd_numbers = 5;
 
     char array_lower_case[] = "abcdefghijklmnopqrstuvwxyz";
@@ -83,9 +97,12 @@ int main(){
     
     int buffer_length = strlen(combined);
     printf("%s (size of combined: %d)\n", combined, buffer_length);
+    shuffle_combined(combined);
+    printf("%s (size of shuffled: %d)\n", combined, buffer_length);
 
     srand(time(NULL));
     for (int i = 0; i < passwd_numbers; i++){
+        shuffle_combined(combined);
         for (int j = 0; j < passwd_length; j++){
             putchar(combined[rand() % buffer_length]);
         }
